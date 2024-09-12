c7763404ccb785fbd75c4d56ff1b404ce9317e2b
h-szyd <szydlowh@ku.edu>
2024-09-12 14:45:40 -0500

    Update 1_AofC_2023_HS.R
    Complete step 1

--- 1_AofC_2023_HS.R
+++ 1_AofC_2023_HS.R
@@ -11,9 +11,32 @@
 # together to create a two-digit number, add the two-digit numbers together to
 # get the final sum of all calibration numbers
 
-# step 1: Get the first digit
-# Step 2: Get the last digit
-# Step 3: Combine the two into one number
-# Step 4: Create a loop to do this for each line and output the two-digit
+# Step 1: Get data and get numbers only
+# step 2: Get the first digit
+# Step 3: Get the last digit
+# Step 4: Combine the two into one number
+# Step 5: Create a loop to do this for each line and output the two-digit
 # value somewhere
-# Step 5: Sum up all two-digit values
+# Step 6: Sum up all two-digit values
+
+setwd("C:/Users/hszyd/Documents/GitHub/Advent-of-Code-2023")
+
+# ---------------------------------------
+
+
+# Step 1: Get data and get numbers only
+# Sample code lines:
+#1abc2 - 12
+#pqr3stu8vwx -38
+#a1b2c3d4e5f -12345
+#treb7uchet - 7
+
+df <- read.csv("SampleData.csv")
+
+# loop to get the numbers only in a separate column
+j <- j+1
+for (j in 0:4) {
+  df$Digits[j] <- gsub("\\D", "", df[j,1])
+}
+
+
