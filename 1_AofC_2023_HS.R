5a6ddcd325736fd21e1c37cdc855adc3ebed1399
h-szyd <szydlowh@ku.edu>
2024-09-12 13:59:20 -0500

    Create 1_AofC_2023_HS.R
    Outline of coding document - no code written yet

---
+++ 1_AofC_2023_HS.R
@@ -0,0 +1,19 @@
+# Advent of Code 2023
+# Hanna Szydlowski
+
+# Day 1: Trebuchet?!
+
+# Problem: Issue with global snow production
+# GOal: Fix the problem by finding the calibration values
+
+
+# Coding Goal: Find the first and last digit of each line, combine the two
+# together to create a two-digit number, add the two-digit numbers together to
+# get the final sum of all calibration numbers
+
+# step 1: Get the first digit
+# Step 2: Get the last digit
+# Step 3: Combine the two into one number
+# Step 4: Create a loop to do this for each line and output the two-digit
+# value somewhere
+# Step 5: Sum up all two-digit values
