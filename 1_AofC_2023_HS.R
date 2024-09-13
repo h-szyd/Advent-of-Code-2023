f960a3aab0b37ccdc229771546f4a18eb950d791
h-szyd <szydlowh@ku.edu>
2024-09-13 15:07:50 -0500

    Update 1_AofC_2023_HS.R
    Attempt at steps 2-4

--- 1_AofC_2023_HS.R
+++ 1_AofC_2023_HS.R
@@ -40,4 +40,43 @@ for (j in 0:4) {
 }
 
 
-# Step 2: Get the first digit
+# step 2: Get the first digit
+# Step 3: Get the last digit
+# Step 4: Combine the two into one number
+# Going to attempt to do this with one if statement within a for loop
+
+#if >2 numbers, take the first and last number
+#if <2 numbers, duplicate the number
+#if 2 numbers, make this the final number
+
+
+# code works when not it a for loop - issue within the loop
+i <- i+1
+for (i in 0:4) {
+  df$Number[i] <-
+    if (nchar(df[i,2]) > 2){
+      print("big")
+      
+    } else if (nchar(df[i,2]) < 2){
+      print("small")
+      
+    } else {
+      print("equal to")
+    }
+}
+
+
+
+
+# this works for a single cell, but breaks in a for loop
+i <- 1
+df$Number[i] <-
+  if (nchar(df[i,2]) > 2){
+    print("big")
+      
+  } else if (nchar(df[i,2]) < 2){
+    print("small")
+      
+  } else {
+    print("equal to")
+  }
